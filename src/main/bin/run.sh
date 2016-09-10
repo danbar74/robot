dfa6ab7261246096e5aa7169189f6061b153a4b7
Daniel Baryla <danbar74@gmail.com>
2016-09-11 02:18:25 +1000

    Fix shell script use to run application: add missing second argument placeholder.

--- src/main/bin/run.sh
+++ src/main/bin/run.sh
@@ -2,4 +2,4 @@
 APP_HOME=`pwd`/..
 OPTIONS="-Dclassworlds.conf=$APP_HOME/bin/application.conf -Dapp.home=$APP_HOME -Dfile.encoding=UTF-8"
 CLASSPATH="$APP_HOME/lib/classworlds-1.0.jar"
-${JAVA_HOME}/bin/java -classpath ${CLASSPATH} ${OPTIONS} org.codehaus.classworlds.Launcher $1
+${JAVA_HOME}/bin/java -classpath ${CLASSPATH} ${OPTIONS} org.codehaus.classworlds.Launcher $1 $2
