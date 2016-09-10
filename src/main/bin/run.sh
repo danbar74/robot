ac56f524f616e7cb1a33817bec6ea4c1d059f5fd
Daniel Baryla <danbar74@gmail.com>
2016-09-11 02:37:19 +1000

    Improve dist version packaging structure.

--- src/main/bin/run.sh
+++ src/main/bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-APP_HOME=`pwd`/..
-OPTIONS="-Dclassworlds.conf=$APP_HOME/bin/application.conf -Dapp.home=$APP_HOME -Dfile.encoding=UTF-8"
+APP_HOME=`pwd`
+OPTIONS="-Dclassworlds.conf=$APP_HOME/conf/application.conf -Dapp.home=$APP_HOME -Dfile.encoding=UTF-8"
 CLASSPATH="$APP_HOME/lib/classworlds-1.0.jar"
 ${JAVA_HOME}/bin/java -classpath ${CLASSPATH} ${OPTIONS} org.codehaus.classworlds.Launcher $1 $2
