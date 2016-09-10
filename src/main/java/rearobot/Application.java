830fdc3624ed645c4179de006feeb7f19fe6969d
Daniel Baryla <danbar74@gmail.com>
2016-09-10 11:13:31 +1000

    Import project: packaging, dependencies, entry point.

---
+++ src/main/java/rearobot/Application.java
@@ -0,0 +1,14 @@
+package rearobot;
+
+import static java.lang.System.out;
+
+public class Application {
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.start();
+    }
+
+    private void start() {
+        out.println("REA Robot.");
+    }
+}
